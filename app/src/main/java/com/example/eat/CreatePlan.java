a5b30980a67677a9303dd7a50f124ffbe0729cfb
Hani Nasuha Yusof <haninasuha@Hanis-Air.lan>
2024-02-16 12:23:53 -0500

    Initial commit

---
+++ app/src/main/java/com/example/eat/CreatePlan.java
@@ -0,0 +1,14 @@
+package com.example.eat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreatePlan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_plan);
+    }
+}
---
+++ app/src/main/java/com/example/eat/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.eat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openCreate(View view) {
+        startActivity(new Intent(this, CreatePlan.class));
+    }
+}
