f1f60ab472f080fa3984e4768ff7f5a648155e58
Mona Albastaki <albastaki.2@buckeyemail.osu.edu>
2024-04-01 12:10:18 -0400

    yippeee merged profile

--- app/src/main/java/com/example/eat/ProfileActivity.java
+++ app/src/main/java/com/example/eat/ProfileActivity.java
@@ -2,19 +2,59 @@ package com.example.eat;
 
 import static androidx.fragment.app.FragmentManager.TAG;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ProfileActivity extends AppCompatActivity {
+    BottomNavigationView navBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         Fragment fragmentProfileDetails = new ProfileFragment();
         this.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_profile, fragmentProfileDetails).commit();
+
+        navBar = findViewById(R.id.nav_bar);
+        navBar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int item_id = item.getItemId();
+
+                if(item_id == R.id.log_out) {
+                    FirebaseAuth.getInstance().signOut();
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return true;
+                } else if (item_id == R.id.home) {
+                    Intent intent = new Intent(getApplicationContext(), ActivePlanActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return true;
+                } else if (item_id == R.id.profile) {
+                    Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return true;
+                } else if (item_id == R.id.plan) {
+                    Intent intent = new Intent(getApplicationContext(), PlanActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
